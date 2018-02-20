c1cdd194616910244bef302842ea54491e46c758
Emma <129emma@gmail.com>
2018-02-20 15:27:53 +1300

    done

---
+++ src/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <table-title>
+    </table-title>
+    <hr>
+    <repeat-times>
+    </repeat-times>
+    <hr>
+    <table-footer>
+    </table-footer>
+  </div>
+</template>
+
+<script>
+import title from './Title.vue';
+import serverStatus from './ServerStatus.vue';
+import footer from './Footer.vue';
+
+export default {
+  components: {
+    'repeat-times': serverStatus,
+    'table-title': title,
+    'table-footer': footer
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HomePage from './Home.vue'
+
+Vue.component('home-component', HomePage);
 
 new Vue({
   el: '#app',
